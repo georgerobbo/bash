91e65bd50e1ce0187b56876ea79025a7babb9dd7
george <george.robertson3@hotmail.co.uk>
2019-04-02 19:55:35 +0100

    complete of rectangle

--- rectangle.sh
+++ rectangle.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 until [[ $restart =~ [Nn][Oo] ]];  do 					# While the users answer to restart is Yes it will continue to run the script
 
 echo "Enter the width of the rectangle:"
@@ -11,19 +10,19 @@ if [[ $width =~ ^[0-9]+[.]?[0-9]*$ ]] && [[ $height =~ ^[0-9]+[.]?[0-9]*$ ]] #ch
 then
 
 	cm=$(echo "$width*$height" | bc) 					# calculates the result in cm
-	inch=$(echo "$width*$height/2.54" | bc) 			# calculates the result in inches
+	inch=$(echo "($width*$height)/2.54" | bc) 			# calculates the result in inches
 
 	echo "The area of the rectangle in cm is:"
 	echo $cm 											# displays the answer in cm
 	echo "The area of the rectangle in inches is"
-	echo $inch 											# displays the answer in inch ONLY SHOWS THE RESULT IN FULL INTEGERS DOESNT DO FLOAT, unsure why
+	echo $inch 											# displays the answer in inch ONLY SHOWS THE RESULT IN FULL INTEGERS DOESN'T DO FLOAT, unsure why
 else
-	echo "Please enter a number"						# if the user doesnt use a actual number it will show this instead of a calculation
+	echo "Please enter a number"	\				# if the user doesnt use a actual number it will show this instead of a calculation
 	
 fi
 
-echo "Do you want to calculate again?"
-echo "Type Yes or no"
+echo "Do you want to calculate again?"	\ 
+echo "Type Yes or no"	
 read restart											# Asks the user if they want to restart which is where the until loop gets it answer from
 
-done
+done  #get error here saying command not found
